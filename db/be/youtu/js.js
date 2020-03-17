2d7b9a7481e6190d9accac302925eb22ccdb5734
mkaraki <backtrack1994+github@gmail.com>
2020-03-18 00:19:36 +0900

    Add some youtube domain youtu.be youtube.com

---
+++ db/be/youtu/js.js
@@ -0,0 +1,17 @@
+// Set FQDN
+setFQDN('youtu.be');
+
+// Owner name (or company name)
+setOwner('Google LLC');
+
+// Owner websize (or company website)
+setOwnerUrl('https://google.com');
+
+// Your name
+setReporter('mkaraki');
+
+// Set Current Date (YYYY/MM/DD)
+setReportDate('2020/03/17');
+
+// Set description (in English)
+setDescription("Link for youtube.com");
