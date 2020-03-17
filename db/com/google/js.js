f0be9928b5fc1b83963cff99ac2b5ee07fbd57fc
mkaraki <backtrack1994+github@gmail.com>
2020-03-17 23:21:26 +0900

    Add some domains google.com google

---
+++ db/com/google/js.js
@@ -0,0 +1,17 @@
+// Set FQDN17Y
+setFQDN('google.com');
+
+// Owner name (or company name)
+setOwner('Google LLC');
+
+// Owner websize (or company website)
+setOwnerUrl('https://about.google/');
+
+// Your name
+setReporter('mkaraki');
+
+// Set Current Date (YYYY/MM/DD)
+setReportDate('2020/03/17');
+
+// Set description (in English)
+setDescription("Top page of google, the search engine.");
---
+++ db/google/js.js
@@ -0,0 +1,17 @@
+// Set FQDN
+setFQDN('google');
+
+// Owner name (or company name)
+setOwner('Alphabet Inc. (Google LLC)');
+
+// Owner websize (or company website)
+setOwnerUrl('https://abc.xyz/');
+
+// Your name
+setReporter('mkaraki');
+
+// Set Current Date (YYYY/MM/DD)
+setReportDate('2020/03/17');
+
+// Set description (in English)
+setDescription("brand TLD");
