70104f96e81d3e111eaaca548da77bfe9e58ec70
mkaraki <backtrack1994+github@gmail.com>
2020-03-18 00:16:04 +0900

    Add twitter.com

---
+++ db/com/twitter/js.js
@@ -0,0 +1,17 @@
+// Set FQDN
+setFQDN('twitter.com');
+
+// Owner name (or company name)
+setOwner('Twitter, Inc.');
+
+// Owner websize (or company website)
+setOwnerUrl('https://twitter.com');
+
+// Your name
+setReporter('mkaraki');
+
+// Set Current Date (YYYY/MM/DD UTC)
+setReportDate('2020/03/17');
+
+// Set description (in English)
+setDescription("");
