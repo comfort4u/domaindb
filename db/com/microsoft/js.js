4a6e7df3200030da3a7324a1e8d5373221e89da0
mkaraki <backtrack1994+github@gmail.com>
2020-03-17 23:44:46 +0900

    Add microsoft.com

---
+++ db/com/microsoft/js.js
@@ -0,0 +1,17 @@
+// Set FQDN
+setFQDN('microsoft.com');
+
+// Owner name (or company name)
+setOwner('Microsoft Corporation');
+
+// Owner websize (or company website)
+setOwnerUrl('https://microsoft.com/');
+
+// Your name
+setReporter('mkaraki');
+
+// Set Current Date (YYYY/MM/DD)
+setReportDate('2020/03/17');
+
+// Set description (in English)
+setDescription("");
