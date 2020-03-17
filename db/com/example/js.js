bc0c0fdd9aaf8db1937a82c917a1c5029bfbe044
mkaraki <backtrack1994+github@gmail.com>
2020-03-17 23:04:26 +0900

    Add example.com

---
+++ db/com/example/js.js
@@ -0,0 +1,17 @@
+// Set FQDN
+setFQDN('example.com');
+
+// Owner name (or company name)
+setOwner('Internet Assigned Numbers Authority');
+
+// Owner websize (or company website)
+setOwnerUrl('https://www.iana.org/');
+
+// Your name
+setReporter('mkaraki');
+
+// Set Current Date (YYYY/MM/DD)
+setReportDate('2020/03/17');
+
+// Set description (in English)
+setDescription("This domain was reserved for examples.");
