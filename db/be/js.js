e6456132e9a64a3e5f071122be79da7bbbc0167f
mkaraki <backtrack1994+github@gmail.com>
2020-03-18 00:22:54 +0900

    Add be

---
+++ db/be/js.js
@@ -0,0 +1,17 @@
+// Set FQDN
+setFQDN('be');
+
+// Owner name (or company name)
+setOwner('DNS Belgium');
+
+// Owner websize (or company website)
+setOwnerUrl('https://www.dnsbelgium.be/');
+
+// Your name
+setReporter('mkaraki');
+
+// Set Current Date (YYYY/MM/DD)
+setReportDate('2020/03/17');
+
+// Set description (in English)
+setDescription("ccTLD");
