1142cc1c4863b1a75b262a175477202c67a400fc
mkaraki <backtrack1994+github@gmail.com>
2020-03-17 23:42:55 +0900

    Add github

---
+++ db/com/github/js.js
@@ -0,0 +1,17 @@
+// Set FQDN
+setFQDN('github.com');
+
+// Owner name (or company name)
+setOwner('GitHub');
+
+// Owner websize (or company website)
+setOwnerUrl('https://github.com');
+
+// Your name
+setReporter('mkaraki');
+
+// Set Current Date (YYYY/MM/DD)
+setReportDate('2020/03/17');
+
+// Set description (in English)
+setDescription("GitHub is a Microsoft's subsidiary");
