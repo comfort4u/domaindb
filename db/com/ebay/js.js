4116bb5548f2290b3ed90acada836750c648b7fc
mkaraki <backtrack1994+github@gmail.com>
2020-03-18 23:12:21 +0900

    Add some domain ebay.com gov

---
+++ db/com/ebay/js.js
@@ -0,0 +1,7 @@
+// js.js for domaindb project.
+setFQDN('ebay.com')
+setOwner("eBay Inc.")
+setOwnerUrl("https://www.ebay.com/")
+setReporter("mkaraki")
+setReportDate('2020/03/18')
+setDescription("")
---
+++ db/gov/js.js
@@ -0,0 +1,7 @@
+// js.js for domaindb project.
+setFQDN('gov')
+setOwner("General Services Administration")
+setOwnerUrl("http://www.gsa.gov/")
+setReporter("mkaraki")
+setReportDate('2020/03/18')
+setDescription("")
